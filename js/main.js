2f90fa008effe690d7512694237347714fa5c57b
roge530 <garu1999@hotmail.com>
2022-08-27 21:11:24 -0500

    Creacion de prototipo para imprimir filtro de area

--- js/main.js
+++ js/main.js
@@ -1,1 +1,35 @@
-console.log('hola')
+var areas = []
+function fnSaludar() {
+    console.log('hola')
+}
+
+function getDataFromApi(urlApi) {
+    return fetch(urlApi)
+    .then(response => {
+        return response.json();
+    })
+}
+
+function fnAllAreas() {
+    getDataFromApi('https://www.themealdb.com/api/json/v1/1/list.php?a=list')
+        .then(data => {
+            areas = data['meals'].map(datum => {
+                return datum['strArea']
+            })
+            console.log(areas.length)
+        })
+}
+fnAllAreas()
+function fnPrintAllAreas() {
+    let fragment = new DocumentFragment()
+    areas = areas.filter(area => area != 'Canadian' || area != 'Mexican')
+    areas.forEach(area => {
+        let div = document.createElement('div')
+        div.setAttribute("id", area)
+        div.setAttribute("onclick", fnSaludar())
+        div.innerHTML = '<p>'+area+'</p>'
+        fragment.appendChild(div)
+    })
+    let divArea = document.querySelector('#areas')
+    divArea.appendChild(fragment)
+}
