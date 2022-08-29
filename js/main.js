853048ed634c6471ddb9209009c8ba6d73a5b05f
roge530 <garu1999@hotmail.com>
2022-08-28 21:15:14 -0500

    incrememento en filtro de areas

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 var areas = []
+var allOpened = false
+
 function fnSaludar() {
     console.log('hola')
 }
@@ -16,20 +18,47 @@ function fnAllAreas() {
             areas = data['meals'].map(datum => {
                 return datum['strArea']
             })
-            console.log(areas.length)
         })
 }
 fnAllAreas()
-function fnPrintAllAreas() {
-    let fragment = new DocumentFragment()
-    areas = areas.filter(area => area != 'Canadian' || area != 'Mexican')
+
+function fnHideAreas() {
+    document.getElementById('allAreas').removeAttribute('hidden')
     areas.forEach(area => {
-        let div = document.createElement('div')
-        div.setAttribute("id", area)
-        div.setAttribute("onclick", fnSaludar())
-        div.innerHTML = '<p>'+area+'</p>'
-        fragment.appendChild(div)
-    })
-    let divArea = document.querySelector('#areas')
-    divArea.appendChild(fragment)
+        document.getElementById(area).setAttribute('hidden', true)
+    });
+    document.getElementById('buttonHidde').setAttribute('hidden', true)    
+}
+
+
+function fnPrintAllAreas() {
+    if (!allOpened) {
+        let fragment = new DocumentFragment()
+        areas = areas.filter(area => area != 'Mexican')
+        areas = areas.filter(area => area != 'Canadian')
+        document.getElementById('allAreas').setAttribute('hidden', true)
+        areas.forEach(area => {
+            let div = document.createElement('div')
+            div.setAttribute("id", area)
+            div.setAttribute("onclick", "fnSaludar()")
+            div.innerHTML = '<p>'+area+'</p>'
+            fragment.appendChild(div)
+        })
+        allOpened = true
+        let divArea = document.querySelector('#areas')
+        divArea.appendChild(fragment)
+        let button = document.createElement('button')
+        button.setAttribute("id", "buttonHidde")
+        button.setAttribute("onclick", "fnHideAreas()")
+        button.innerHTML = 'Ocultar'
+        let areasSection = document.querySelector('#areasSection')
+        areasSection.appendChild(button)
+    }
+    else {
+        document.getElementById('allAreas').setAttribute('hidden', true)
+        areas.forEach(area => {
+            document.getElementById(area).removeAttribute('hidden')
+        });
+        document.getElementById('buttonHidde').removeAttribute('hidden')
+    }
 }
